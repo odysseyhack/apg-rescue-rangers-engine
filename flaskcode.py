9c1c682934d7f52b950dd2e5c018dd95cbba2cac
bart <bart-driessen@hotmail.com>
2019-04-14 10:58:08 +0200

    Last adjustments API

--- flaskcode.py
+++ flaskcode.py
@@ -10,15 +10,52 @@ cors = CORS(app)
 # app.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin'
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-# @app.route("/topicmodels")
-# @cross_origin()
-# def sendTopicModels():
-#     return jsonify(historicalTwitter.getTexts())
+QuestionIndicator = False
+AnswerIndicator = False
 
-# @app.route("/images")
-# @cross_origin()
-# def sendImages():
-# 	return jsonify(historicalTwitter.getImages())
+QList = "" 
+AList = 0
+
+@app.route("/getQuestion")
+@cross_origin()
+def getQuestion():
+	global QuestionIndicator
+	global QList
+	QuestionIndicator = False
+	return jsonify({'results':QList})
+
+@app.route("/getAnswer")
+@cross_origin()
+def getAnswer():
+	global AnswerIndicator
+	global AList
+	AnswerIndicator = False
+	return jsonify({'results':AList})
+
+@app.route("/updateQuestion")
+@cross_origin()
+def updateQuestion():
+	global QList
+	QList = request.args.get('QList')
+	return jsonify({'results':"success"})
+
+@app.route("/updataAnswer")
+@cross_origin()
+def updataAnswer():
+	global AList
+	AList =  request.args.get('AList') 
+	return  jsonify({'results':"success"})
+
+@app.route("/getQIndicator")    
+@cross_origin()
+def getQIndicator():
+	return  jsonify({'results':QuestionIndicator})
+
+@app.route("/getAIndicator")
+@cross_origin()
+def getAIndicator():
+	return  jsonify({'results':AnswerIndicator})
+
 
 @app.route("/sendtags")
 @cross_origin()
